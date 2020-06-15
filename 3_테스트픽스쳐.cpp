ee9e7cf340956f87139b25b440eead97608f8055
Chansik Yun <chansigi@gmail.com>
2020-06-15 04:27:48 +0000

    .

---
+++ 3_테스트픽스쳐.cpp
@@ -0,0 +1,41 @@
+class Calculator {
+public:
+	double Display() { return 0; }
+
+	void Enter(double v) {}
+	void PressPlus() {}
+	void PressMinus() {}
+	void PressEquals() {}
+
+	Calculator() {}
+};
+//-------------------------------------
+#include <gtest/gtest.h>
+
+TEST(CalculatorTest, PlusTest) {
+	Calculator* calc = new Calculator;
+
+	calc->Enter(2);
+	calc->PressPlus();
+	calc->Enter(30);
+	calc->PressEquals();
+
+	if (calc->Display() == 32) {
+		SUCCEED();
+	} else {
+		FAIL() << "2 + 30 하였을 때";
+	}
+}
+
+#define SPEC printf
+TEST(CalculatorTest, PlusTest_2Plus30_Displays_32) {
+	SPEC("2와 30을 더했을 때의 동작\n");
+	Calculator* calc = new Calculator;
+
+	calc->Enter(2);
+	calc->PressPlus();
+	calc->Enter(30);
+	calc->PressEquals();
+
+	ASSERT_EQ(32, calc->Display()) << "2 + 30 하였을 때";
+}
